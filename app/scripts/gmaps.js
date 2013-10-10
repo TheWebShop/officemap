424830069ed4d65de3aa9e534d8a3bdc236e4208
Kevin Attfield <k.attfield@gmail.com>
2013-10-10 14:33:10 -0700

    google maps async

---
+++ app/scripts/gmaps.js
@@ -0,0 +1,7 @@
+/*global require*/
+'use strict';
+
+define('gmaps', ['async!http://maps.google.com/maps/api/js?v=3&sensor=false'],
+function(){
+    return window.google.maps;
+});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,12 +17,15 @@ require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
         backbone: '../bower_components/backbone/backbone',
-        underscore: '../bower_components/underscore/underscore'
-    }
+        underscore: '../bower_components/underscore/underscore',
+        async: '../bower_components/requirejs-plugins/src/async'
+    },
+    waitSeconds : 120 //make sure it is enough to load all gmaps scripts
 });
 
 require([
-    'backbone'
-], function (Backbone) {
+    'backbone',
+    'gmaps'
+], function (Backbone, gmaps) {
     Backbone.history.start();
 });
