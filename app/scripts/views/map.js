bbe448f609005b8e15ea7c9caafefdce01bc145e
Kevin Attfield <k.attfield@gmail.com>
2013-10-10 15:28:20 -0700

    updated map options

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -12,9 +12,23 @@ define([
         el: $("#map")[0],
         $el: $("#map"),
         mapOptions: {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            center: new google.maps.LatLng(53.7266683, -127.64762059999998),
+            zoom: 6,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+                position: google.maps.ControlPosition.BOTTOM_CENTER
+            },
+            panControl: true,
+            panControlOptions: {
+                position: google.maps.ControlPosition.TOP_RIGHT
+            },
+            zoomControl: true,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.LARGE,
+                position: google.maps.ControlPosition.RIGHT_TOP
+            },
         },
         render: function() {
             this.map = new google.maps.Map(this.el, this.mapOptions);
