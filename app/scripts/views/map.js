2af5c190bf01b851706a89dd6a21d2444c6ce02a
Kevin Attfield <k.attfield@gmail.com>
2013-10-17 15:30:27 -0700

    map control style match

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -50,25 +50,12 @@ define([
                 // Set CSS styles for the DIV containing the control
                 // Setting padding to 5 px will offset the control
                 // from the edge of the map
-                controlDiv.style.padding = '5px';
+                controlDiv.style.margin = '5px';
 
-                // Set CSS for the control border
-                var controlUI = document.createElement('div');
-                controlUI.style.backgroundColor = 'white';
-                controlUI.style.borderStyle = 'solid';
-                controlUI.style.borderWidth = '2px';
-                controlUI.style.cursor = 'pointer';
-                controlUI.style.textAlign = 'center';
-                controlUI.title = 'Click to set the map to Home';
+                var controlUI = $('<div class="control" title="Click to set show/hide office list">')[0];
                 controlDiv.appendChild(controlUI);
 
-                // Set CSS for the control interior
-                var controlText = document.createElement('div');
-                controlText.style.fontFamily = 'Arial,sans-serif';
-                controlText.style.fontSize = '12px';
-                controlText.style.paddingLeft = '4px';
-                controlText.style.paddingRight = '4px';
-                controlText.innerHTML = '<b>Offices</b>';
+                var controlText = $('<div class="control-button">Offices</div>')[0];
                 controlUI.appendChild(controlText);
 
                 google.maps.event.addDomListener(controlUI, 'click', function() {
